0d90aa3e0b7b868b80cec6bd2fd870d548dbb329
Jaarcoo <marco.binz@stud.fhgr.ch>
2023-12-01 09:13:38 +0100

    javascript funktionen (vergangene Kalenderwochen nicht klickbar etc.)

--- js/lebensKalender.js
+++ js/lebensKalender.js
@@ -1,71 +1,96 @@
-const calendar = document.getElementById("calendar");
+document.addEventListener('DOMContentLoaded', function() {
+  const calendar = document.getElementById("calendar");
 
-// Lebenserwartung (zum Beispiel 100 Jahre)
-const verticalBlocks = 10;
-const verticalWeekBlock = 10
+  // Lebenserwartung (zum Beispiel 100 Jahre)
+  const verticalBlocks = 10;
+  const verticalWeekBlock = 10;
 
-// Berechne die Anzahl der Wochen im Lebenskalender
-const totalBlocks = verticalBlocks * 13;
-const totalWeeks = verticalWeekBlock * 4;
+  // Berechne die Anzahl der Wochen im Lebenskalender
+  const totalBlocks = verticalBlocks * 13;
+  const totalWeeks = verticalWeekBlock * 4;
 
-// Erstelle die Wochen im Lebenskalender
-for (let block = 1; block <= totalBlocks; block++) {
+  // Erstelle die Wochen im Lebenskalender
+  for (let blockNum = 1; blockNum <= totalBlocks; blockNum++) {
     const block = document.createElement("div");
-    block.className = "block"; 
-
+    block.className = "block";
+  
     for (let week = 1; week <= totalWeeks; week++) {
-        const weekElement = document.createElement("div");
-        weekElement.className = "week"; 
-
-        weekElement.addEventListener("click", function() {
-          window.location.href = '/wochenReflexion.html';
-      });
-
-        block.appendChild(weekElement);
+      const weekElement = document.createElement("div");
+      weekElement.className = "week";
+      let weekId = "week-" + blockNum + "-" + week;
+      weekElement.id = weekId;
+  
+      // Use the new function to add click listener
+      addClickListenerIfCurrentWeek(weekElement, week);
+  
+      block.appendChild(weekElement);
     }
-    
-    // Eventlistener um Wochen hinzuzufügen
-    // weekElement.addEventListener("click", function() {
-    //     if (weekElement.classList.contains("marked")) {
-    //         weekElement.classList.remove("marked");
-    //     } else {
-    //         weekElement.classList.add("marked");
-    //     }
+  
     calendar.appendChild(block);
+  }
 
-    }
-    ;
 
-//Wochen in der Vergangenheit mit random Farben zuweisen
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Ersetze 'birthdateInput' mit der ID deines Geburtsdatum-Eingabefelds
-        const birthdateInput = document.getElementById('birthdate');
-        const submitBtn = document.getElementById('submitBtn');
-        
-        submitBtn.addEventListener('click', function() {
-          const birthdate = new Date(birthdateInput.value);
-          colorPastWeeks(birthdate);
-        });
-      });
-      
-      function colorPastWeeks(birthdate) {
-        const today = new Date();
-        const oneWeek = 1000 * 60 * 60 * 24 * 7;
-        const weeksLived = Math.floor((today - birthdate) / oneWeek);
-        const weeks = document.querySelectorAll('.week');
-      
-        for (let i = 0; i < weeksLived && i < weeks.length; i++) {
-          weeks[i].style.backgroundColor = getRandomColor();
-        }
-      }
-      
-      function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-      }
-    
+
+  // Get birthdate from localStorage and color past weeks
+  colorPastWeeks();
+  //colorCurrentWeek();
+});
+
+//  if (isCurrentWeek(week)) {
+//     weekElement.addEventListener("click", function() {
+//     window.location.href = '/wochenReflexion.html?' + weekId;
+
+//     });
+//   }
+
+function addClickListenerIfCurrentWeek(weekElement, weekNumber) {
+  if (isCurrentWeek(weekNumber)) {
+    weekElement.addEventListener("click", function() {
+      window.location.href = '/wochenReflexion.html?' + weekElement.id;
+    });
+  }
+}
+
+function isCurrentWeek(weekNumber) {
+  const userDataString = localStorage.getItem('userData');
+  if (!userDataString) return false;
+
+  const userData = JSON.parse(userDataString);
+  const birthdate = new Date(userData.date);
+
+  const today = new Date();
+  const oneWeek = 1000 * 60 * 60 * 24 * 7;
+  const currentWeek = Math.floor((today - birthdate) / oneWeek);
+  const differenz = currentWeek - userData.weeksLived;
+  console.log(differenz);
+}
+
+
+function colorPastWeeks() {
+  const userDataString = localStorage.getItem('userData');
+  if (!userDataString) return;
+
+  const userData = JSON.parse(userDataString);
+  const weeksLived = userData.weeksLived;
+
+  const weeks = document.querySelectorAll('.week');
+
+  for (let i = 0; i < weeksLived && i < weeks.length; i++) {
+      weeks[i].style.backgroundColor = "#000";
+      weeks[i].style.pointerEvents = "none";
+  }
+}
+
+
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+
+
